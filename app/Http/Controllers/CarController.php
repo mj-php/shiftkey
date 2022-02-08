a939f933d1b1a0a5b09ee4f0b372e266f30e9503
Michał <michalj@godigitally.io>
2022-02-08 21:58:54 +0100

    added controllers for cars and trips

---
+++ app/Http/Controllers/CarController.php
@@ -0,0 +1,95 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Car;
+use App\Http\Requests\StoreCar;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class CarController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param StoreCar $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreCar $request)
+    {
+        $validated = $request->validated();
+
+        $car = Auth::user()->cars()->create($validated);
+
+        return response()->json($car, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        return Car::findOrFail($id);
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Car $car)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Car  $car
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Car $car)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int $id
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $car = Car::findOrFail($id);
+        $car->trips()->delete();
+        $car->delete();
+
+        return response()->json('Car with ID = ' . $car->id . ' was deleted.', 201);
+    }
+}
---
+++ app/Http/Controllers/TripController.php
@@ -0,0 +1,91 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\StoreTrip;
+use App\Trip;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
+
+class TripController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  StoreTrip $request
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function store(StoreTrip $request)
+    {
+        $validated = $request->validated();
+
+        $trip = Trip::create($validated);
+
+        return response()->json($trip, 201);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Trip $trip)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Trip  $trip
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Trip $trip)
+    {
+        //
+    }
+}
