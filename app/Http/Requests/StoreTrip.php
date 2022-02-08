41d02a4b797850a5ec08108aeb79f24073f0dc52
Michał <michalj@godigitally.io>
2022-02-08 21:59:40 +0100

    added request for storing cars and trips

---
+++ app/Http/Requests/StoreTrip.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class StoreTrip extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'date' => 'required|date', // ISO 8601 string
+            'car_id' => 'required|integer',
+            'miles' => 'required|numeric'
+        ];
+    }
+}
